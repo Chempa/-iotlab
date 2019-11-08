81132d25570aca103930c43adba07d634181dff8
Chempa <30638759+Chempa@users.noreply.github.com>
2019-11-08 15:33:11 +0000

    Update updatedoor.php

--- updatedoor.php
+++ updatedoor.php
@@ -1,11 +1,11 @@
 <?php
 echo "Hello World";
 include("db.php");
-// function updateDoorStatus($conn,$opened){
-// 	$query = "UPDATE iotdoor SET opened = '$opened' WHERE doornumber = 1;";
-// 	$result = mysqli_query($conn,$query) or die(mysql_error());
-// 	return 1
-// }
+function updateDoorStatus($conn,$opened){
+	$query = "UPDATE iotdoor SET opened = '$opened' WHERE doornumber = 1;";
+	$result = mysqli_query($conn,$query) or die(mysql_error());
+	return 1
+}
 // $state = $_GET["opened"];
 // $token = $_GET["token"];
 // if($token != "IOTLAB.UCC.DOOR.1234567890.qwertyuiop.asdfghjkl.zxcvbnm.client"){
