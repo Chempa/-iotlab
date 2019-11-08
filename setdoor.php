5376c440749540d775979123c0d763f7ae9cbda4
Chempa <30638759+Chempa@users.noreply.github.com>
2019-11-08 15:14:54 +0000

    Update setdoor.php

--- setdoor.php
+++ setdoor.php
@@ -1,17 +1,18 @@
 <?php 
-  include("db.php");
-  function updateDoorStatus($conn,$opened){
+include("db.php");
+function updateDoorStatus($conn,$opened){
 	$query = "UPDATE iotdoor SET opened = '$opened' WHERE doornumber = 1;";
 	$result = mysqli_query($conn,$query) or die(mysql_error());
 	return 1
 }
-  $state = $_GET["opened"];
-  $token = $_GET["token"];
-  if($token != "IOTLAB.UCC.DOOR.1234567890.qwertyuiop.asdfghjkl.zxcvbnm.server"){
-    echo "authentication failed: wrong token";
-    exit();
-  }
-  updateDoorStatus($con, $state)
-  echo "success";
-  exit();
+$state = $_GET["opened"];
+$token = $_GET["token"];
+if($token != "IOTLAB.UCC.DOOR.1234567890.qwertyuiop.asdfghjkl.zxcvbnm.client"){
+	echo "authentication failed: wrong token";
+	exit();
+}
+updateDoorStatus($con, $state)
+echo "success";
+exit();
+
 ?>
