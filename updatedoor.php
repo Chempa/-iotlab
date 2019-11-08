e37846175b02a5384fa1b2d2589aa08e185264e3
Chempa <30638759+Chempa@users.noreply.github.com>
2019-11-08 15:36:35 +0000

    Update updatedoor.php

--- updatedoor.php
+++ updatedoor.php
@@ -1,10 +1,10 @@
 <?php
 echo "Hello World";
 include("db.php");
-function updateDoorStatus($conn,$opened){
+function setDoorState($conn, $opened){
 	$query = "UPDATE iotdoor SET opened = '$opened' WHERE doornumber = 1;";
 	$result = mysqli_query($conn,$query) or die(mysql_error());
-	return 1
+	return 1;
 }
 // $state = $_GET["opened"];
 // $token = $_GET["token"];
