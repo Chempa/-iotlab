174023536d0fa5e2930e6d3c30e081aea6057226
Chempa <30638759+Chempa@users.noreply.github.com>
2019-11-08 14:44:15 +0000

    Update getdoor.php

--- getdoor.php
+++ getdoor.php
@@ -1,26 +1,26 @@
 <?php 
-  include("db.php");
-  function updateDoorStatus($conn,$opened){
-			$query = "UPDATE iotdoor SET opened = '$opened' WHERE doornumber = 1;";
-			$result = mysqli_query($conn,$query) or die(mysql_error());
-			if ($conn->query($sql) === TRUE) {
-            return 1;
-//           echo "Record updated successfully";
-      } else {
-//           echo "Error updating record: " . $conn->error;
-            return 0;
-      }
+include("db.php");
+function getDoorStatus($conn){
+	$query = "SELECT * from iotdoor WHERE doornumber = 1;";
+	$result = mysqli_query($conn,$query) or die(mysql_error());
+	$rows = mysqli_num_rows($result);
+		if($row = mysqli_fetch_assoc($result)){
+			return strval($row["doornumber"]);
+		}else{
+			return "-1";
 		}
-  $state = $_GET["opened"];
-  $token = $_GET["token"];
-  if($token == "IOTLAB.UCC.DOOR.1234567890.qwertyuiop.asdfghjkl.zxcvbnm"){
-    echo "authentication failed: wrong token";
-    exit();
-  }
-  if(updateDoorStatus($con, $state) == 0){
-    echo "Update door status failed";
-  }else{
-    echo "Update success";
-  }
-  exit();
+			
+}
+$token = $_GET["token"];
+if($token == "IOTLAB.UCC.DOOR.1234567890.qwertyuiop.asdfghjkl.zxcvbnm.client"){
+	echo "authentication failed: wrong token";
+	exit();
+}
+$ret = getDoorStatus($con);
+if($ret == "-1"){
+	echo "-1";
+}else{
+	echo $ret;
+}
+exit();
 ?>
