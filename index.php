2af14add0482eb39888b1716c481f9adac372ad7
Chempa <30638759+Chempa@users.noreply.github.com>
2019-11-08 15:38:04 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 	<title></title>
 </head>
 <body>
-<!-- 	<form method="get" action="./getdoor.php">
+	<form method="get" action="./getdoor.php">
 		<input type="text" name="token" value="IOTLAB.UCC.DOOR.1234567890.qwertyuiop.asdfghjkl.zxcvbnm.client">
 		<button type="submit">Submit</button>
-	</form> -->
-	<form method="get" action="./updatedoor.php">
+	</form>
+	<form method="get" action="./setdoor.php">
 		<input type="text" name="token" value="IOTLAB.UCC.DOOR.1234567890.qwertyuiop.asdfghjkl.zxcvbnm.client">
 		<input type="text" name="opened" value="1">
 		<button type="submit">Submit</button>
